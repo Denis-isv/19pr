27e67d0f964f60eb38b6304556099de81e0cf02d
student-v424 <student-v424@STUD-RM5-V424>
2026-05-21 14:56:08 +0500

    Создание и подключение бд. Останин

---
+++ KINO_Останин/Classes/Common/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Kino_Останин.Classes.Common
+{
+    public class Connection
+    {
+        private static readonly string config = "server=127.0.0.1;uid=root;pwd=;database=kino;";
+
+        public MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
